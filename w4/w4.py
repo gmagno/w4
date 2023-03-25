c1b958d80de276f548bdafd211b1a7de5a56d753
Gonçalo Magno <goncalo.magno@gmail.com>
2023-03-25 14:29:38 +0000

    Add solutions_matrix example

--- w4/w4.py
+++ w4/w4.py
@@ -18,6 +18,18 @@ def solution_dtype(dim: int) -> np.dtype:
     return dtype
 
 
+def solutions_matrix_dtype(dim: int) -> np.dtype:
+    dtype: np.dtype = np.dtype(
+        [
+            ("iteration", np.uint64),
+            ("error", np.float64),
+            *((f"x{n}", np.float64) for n in range(dim)),  # final value
+            *((f"xi{n}", np.float64) for n in range(dim)),  # initial condition
+        ]
+    )
+    return dtype
+
+
 def w4(
     x0: np.ndarray,
     f: Callable[[np.ndarray], np.ndarray],
@@ -52,10 +64,47 @@ def w4(
 
         solution[i] = (i, error, *x)
 
-        if error < errmax:
+        if error < errmax or np.isnan(x).any() or np.isnan(x).any():
             break
 
     return solution[(0 if trace else i) : i + 1]
+
+
+def w4_matrix(
+    width: int,
+    height: int,
+    xmin: float,
+    xmax: float,
+    ymin: float,
+    ymax: float,
+    f: Callable[[np.ndarray], np.ndarray],
+    fa: Callable[[np.ndarray], np.ndarray],
+    jac: Callable[[np.ndarray], np.ndarray],
+    dt: float = 0.5,
+    maxiter: int = 1000,
+    errmax: float = 1e-4,
+    decomposition: Decomposition = Decomposition.LU,
+) -> np.ndarray:
+    h: int = height  # + 1
+    w: int = width  # + 1
+    solutions_matrix: np.ndarray = np.zeros((h, w), dtype=solutions_matrix_dtype(2))
+    for i in range(h):
+        for j in range(w):
+            x: float = xmin + (xmax - xmin) / width * j
+            y: float = ymax - (ymax - ymin) / height * i
+            solution: np.ndarray = w4(
+                x0=np.array((x, y)),
+                f=f,
+                fa=fa,
+                jac=jac,
+                dt=dt,
+                maxiter=maxiter,
+                errmax=errmax,
+                decomposition=decomposition,
+                trace=False,
+            )
+            solutions_matrix[i, j] = (*solution[0], x, y)
+    return solutions_matrix
 
 
 def main() -> None:
@@ -76,6 +125,20 @@ def main() -> None:
         x0=x0, f=f, fa=fa, jac=jac, decomposition=Decomposition.LU, trace=True
     )
     print(solution)
+
+    solutions_matrix: np.ndarray = w4_matrix(
+        width=10,
+        height=10,
+        xmin=-5.0,
+        xmax=5.0,
+        ymin=-5.0,
+        ymax=5.0,
+        f=f,
+        fa=fa,
+        jac=jac,
+        decomposition=Decomposition.LU,
+    )
+    print(solutions_matrix)
 
 
 if __name__ == "__main__":
